89e8d87898697d3961540f4da8eabff355cfbc73
Suneelk1 <suneel@ashmar.in>
2021-08-31 17:16:15 +0530

    intial commit

---
+++ src/constants/questionarray.js
@@ -0,0 +1,44 @@
+let questionsArray =[
+    {
+        question: "What is your name ?",
+        resumeField:"Name",
+        resumeFieldId:"name"
+    },
+    {
+        question:"Email address",
+        resumeField:"Email",
+        resumeFieldId:"email"
+    },
+    {
+        question:"Phone Number",
+        resumeField:"Phone Number",
+        resumeFieldId:"phoneNumber"
+    },
+    {
+        question:"Enter your Address",
+        resumeField:"Address",
+        resumeFieldId:"address"
+    },
+    {
+        question:"What is your carrer objective",
+        resumeField:"Career Objective",
+        resumeFieldId:"objective"
+    },
+    {
+        question:"What are your skills and abilities",
+        resumeField:"Skills",
+        resumeFieldId:"skills"
+    },
+    {
+        question:"Work Exprience",
+        resumeField:"Work Exprience",
+        resumeFieldId:"exprience"
+    },
+    {
+        question:"Education",
+        resumeField:"Educational Qualifications",
+        resumeFieldId:"education"
+    },
+];
+
+export default questionsArray;
